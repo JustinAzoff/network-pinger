3d92de65bbd9b25847664da042f713429e527aa5
Justin Azoff <justin@bouncybouncy.net>
2009-07-28 13:08:10 -0400

    remove stomp stuff from depends

--- setup.py
+++ setup.py
@@ -17,9 +17,6 @@ setup(
     install_requires=[
         "Pylons>=0.9.7",
         "SQLAlchemy>=0.5",
-        "Twisted",
-        "Orbited",
-        "stomp",
     ],
     setup_requires=["PasteScript>=1.6.3"],
     packages=find_packages(exclude=['ez_setup']),
