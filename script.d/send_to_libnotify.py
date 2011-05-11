a348d481acd13a0a2d13be048f48309f19dcad1c
Justin Azoff <justin@bouncybouncy.net>
2011-05-11 15:42:31 -0400

    example scripts

--- script.d/send_to_libnotify.py
+++ script.d/send_to_libnotify.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python
 import simplejson
+import subprocess
 import os
 import sys
-
-import pygtk
-pygtk.require('2.0')
-import pynotify
-
-if not pynotify.init("Network Pinger"):
-    sys.exit(1)
-
 
 d =  os.getenv("ALERTS")
 alerts = simplejson.loads(d)
@@ -20,8 +13,4 @@ num_hosts = len(alerts)
 status =  sys.argv[1]
 
 msg="Hosts are %s - %d - %s" % (status, num_hosts, hosts)
-n = pynotify.Notification(msg)
-n.update("Alerts", msg)
-n.set_hint("x", 1050)
-n.set_hint("y", 880)
-n.show()
+subprocess.call(["notify-send", "Alerts", msg])
